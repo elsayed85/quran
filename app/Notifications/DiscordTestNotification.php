182f00521051f658ec168f13d59a89a6411f5841
unknown <elsayedkamal581999@gmail.com>
2021-04-07 22:08:16 +0200

    “wip”

---
+++ app/Notifications/DiscordTestNotification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Discord\DiscordChannel;
+use NotificationChannels\Discord\DiscordMessage;
+
+class DiscordTestNotification extends Notification
+{
+    use Queueable;
+    public $password;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [DiscordChannel::class];
+    }
+
+    public function toDiscord($notifiable)
+    {
+        return DiscordMessage::create("thank you *{$notifiable->name}* for being part of our channel , your *password* is **{$this->password}**");
+    }
+}
